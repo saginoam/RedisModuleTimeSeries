34f12062095d872bed45aeddbed5abc046915f0f
noam <saginoam@gmail.com>
2016-11-19 16:50:19 +0200

    use also es 5. Its actually slower. but 50% less memory usage.

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -3,10 +3,12 @@ from elasticsearch import Elasticsearch
 import redis
 
 tskey = "pytsbench"
-es = Elasticsearch()
+es_1_7 = Elasticsearch(["localhost:9200"])
+es_5_0 = Elasticsearch(["localhost:9222"])
 client = redis.Redis()
 
 num_entries = 3
+
 
 def info():
     '''
@@ -26,7 +28,7 @@ def info():
     '''
 
 
-def delete_all():
+def delete_all(es):
     try:
         es.indices.delete(tskey)
     except:
@@ -63,35 +65,51 @@ def add_redis_entry(i, day, hour):
         client.execute_command('TS.INSERT', key + "_usage_time", str(i * 0.2 * e), timestamp)
 
 
-def add_es_entry(i, day, hour):
+def add_es_entry(i, day, hour, use_5_0 = False):
     timestamp = get_timestamp(day, hour, 0)
+    prefix = "params." if use_5_0 else ""
     user_id = "user_id_%d" % (i)
     dev_id = "device_id_%d" % (i)
     key = "%s_%s" % (user_id, dev_id)
     script = "ctx._source.count += 1; "
-    script += "ctx._source.storage_used += storage_used; "
-    script += "ctx._source.pages_visited += pages_visited; "
-    script += "ctx._source.usage_time += usage_time; "
+    script += "ctx._source.storage_used += %sstorage_used; " % (prefix)
+    script += "ctx._source.pages_visited += %spages_visited; "  % (prefix)
+    script += "ctx._source.usage_time += %susage_time; "  % (prefix)
+
     for e in range(1, num_entries + 1):
-        es.update(index=tskey, doc_type=tskey, id=key + "_" + timestamp, body={
+        params = {
+            "storage_used": i * 1.1 * e,
+            "pages_visited": i * e,
+            "usage_time": i * 0.2 * e
+        }
+        upsert = {
+            "user_id": user_id,
+            "device_id": dev_id,
+            "username": "username%d" % (i),
+            "email": "username%d@timeseries.com" % (i),
+            "account": "standard",
+            "count": 1
+        }
+        upsert.update(params)
+        script_1_7 = {
             "script": script,
-            "params": {
-                "storage_used": i * 1.1 * e,
-                "pages_visited": i * e,
-                "usage_time": i * 0.2 * e
+            "params": params,
+            "upsert": upsert
+        }
+        script_5_0 = {
+            "script": {
+                "inline": script,
+                "lang": "painless",
+                "params": params
             },
-            "upsert": {
-                "storage_used": i * 1.1 * e,
-                "pages_visited": i * e,
-                "usage_time": i * 0.2 * e,
-                "user_id": user_id,
-                "device_id": dev_id,
-                "username": "username%d" % (i),
-                "email": "username%d@timeseries.com" % (i),
-                "account": "standard",
-                "count": 1
-            }
-        })
+            "upsert": upsert
+        }
+        body = script_5_0 if use_5_0 else script_1_7
+        es = es_5_0 if use_5_0 else es_1_7
+        es.update(index=tskey, doc_type=tskey, id=key + "_" + timestamp, body=body)
+
+def add_es_entry_5_0(i, day, hour):
+    add_es_entry(i, day, hour, True)
 
 
 def sizeof_fmt(num, suffix='b'):
@@ -108,7 +126,8 @@ def get_redis_size():
         redis_size += client.execute_command('DEBUG OBJECT', key)['serializedlength']
     return "size: %s (%d)" % (sizeof_fmt(redis_size), redis_size)
 
-def get_es_size():
+
+def get_es_size(es):
     ind = 0
     try:
         ret = es.indices.stats(tskey)
@@ -116,6 +135,12 @@ def get_es_size():
     except:
         pass
     return "size: %s (%d)" % (sizeof_fmt(ind), ind)
+
+def get_es_size_1_7():
+    return get_es_size(es_1_7)
+
+def get_es_size_5_0():
+    return get_es_size(es_5_0)
 
 def run_for_all(size, cb, msg, size_cb):
     start = datetime.now().replace(microsecond=0)
@@ -129,20 +154,21 @@ def run_for_all(size, cb, msg, size_cb):
 
 def do_benchmark(size):
     print "delete data"
-    delete_all()
+    delete_all(es_1_7)
+    delete_all(es_5_0)
     print "----------------------------------------"
     print "benchmark size: ", size
-    run_for_all(size, add_redis_entry, "redis", get_redis_size)
-    run_for_all(size, add_es_entry, "es   ", get_es_size)
+    run_for_all(size, add_redis_entry,  "redis", get_redis_size)
+    run_for_all(size, add_es_entry,     "es1_7", get_es_size_1_7)
+    run_for_all(size, add_es_entry_5_0, "es5_0", get_es_size_5_0)
     print "----------------------------------------"
 
 
 do_benchmark(1)
 do_benchmark(10)
 # do_benchmark(100)
-#do_benchmark(1000)
+# do_benchmark(1000)
 
 # TODO benchmark get api
 # TODO Use bulk api
-# TODO Compare also es 5.0 with painless script (should be much faster)
 # TODO Compare also with timeseries over redis hset and not my own custom type (memory usage much higher)
