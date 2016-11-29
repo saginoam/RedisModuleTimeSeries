266615fe6beb47c9a5a71d7148ea33a9719ddd12
noam <saginoam@gmail.com>
2016-11-29 18:54:24 +0200

    stream demo

--- benchmark/redis_ts.py
+++ benchmark/redis_ts.py
@@ -1,3 +1,5 @@
+import random
+
 import redis
 import json
 
@@ -10,11 +12,21 @@ client.execute_command('TS.CREATEDOC', "tsdoctest", json.dumps({
         "ts_fields": ["pagesVisited", "storageUsed", "trafficUsed"]
     }))
 
-client.execute_command('TS.INSERTDOC', "tsdoctest", json.dumps({
-        "userId": "uid1",
-        "deviceId": "devid1",
-        "pagesVisited": 1,
-        "storageUsed": 2,
-        "trafficUsed": 3,
-        "timesamp": "2016:01:01 00:01:00"
-    }))
+def doc(userId, deviceId, hour, minute):
+    return json.dumps({
+        "userId": userId,
+        "deviceId": deviceId,
+        "pagesVisited": random.randint(1, 10),
+        "storageUsed": random.randint(1, 10),
+        "trafficUsed": random.randint(1, 10),
+        "timestamp": "2016:01:01 %.2d:%.2d:00" % (hour, minute)
+    })
+
+# Simulate a data stream such as logstash with input kafka output redis
+for hour in range(0, 24):
+    for minute in range(0, 60, 5):
+        client.execute_command('TS.INSERTDOC', "tsdoctest", doc("user1", "deviceA", hour, minute))
+        client.execute_command('TS.INSERTDOC', "tsdoctest", doc("user1", "deviceA", hour, minute))
+        client.execute_command('TS.INSERTDOC', "tsdoctest", doc("user1", "deviceB", hour, minute))
+        client.execute_command('TS.INSERTDOC', "tsdoctest", doc("user2", "deviceB", hour, minute))
+        client.execute_command('TS.INSERTDOC', "tsdoctest", doc("user2", "deviceC", hour, minute))
